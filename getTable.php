bd3e2e7d713ad1f395e806a99b1347d079c0c98c
MartinBarker <martinbarker99@gmail.com>
2018-10-28 23:13:14 -0700

    more work, errors, trying to connect to myphpadmin database online

--- getTable.php
+++ getTable.php
@@ -1,9 +1,33 @@
 <?php
+
+$servername = "localhost";//"classmysql";
+$username = "cs340_barkemar";
+$password = "4778";
+$dbname = "Concert";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection 
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM Concert";
+/*
+if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+*/
+
 $q=$_GET["q"];
 
+/*
 $xmlDoc = new DOMDocument();
 $xmlDoc->load("cd_catalog.xml");
-/*
+
 $x=$xmlDoc->getElementsByTagName('ARTIST');
 
 for ($i=0; $i<=$x->length-1; $i++) {
@@ -27,4 +51,6 @@ for ($i=0;$i<$cd->length;$i++) {
 }
 */
 echo("<b>ayylmao:</b> ");
+//mysqli_close($conn);
+$conn->close();
 ?>
